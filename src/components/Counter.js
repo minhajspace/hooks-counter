230e9b7d80ecbe490dc538fb12a2d4cb5a5102a3
minhajspace <minhaj.space@gmail.com>
2020-01-20 20:24:35 +0530

    adding prevCount function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -19,8 +19,8 @@ const Counter = () => {
 
                 <div className="extra content">
                     <div className="ui two buttons">
-                        <div className="ui basic green button" onClick={() => setCounter(counter + 1)} >Increment</div>
-                        <div className="ui basic red button" onClick={() => setCounter(counter - 1)}>Decrement</div>
+                        <div className="ui basic green button" onClick={() => setCounter(prevCount => prevCount + 1)} >Increment</div>
+                        <div className="ui basic red button" onClick={() => setCounter(prevCount => prevCount - 1)}>Decrement</div>
 
 
                     </div>
