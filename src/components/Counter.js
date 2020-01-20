bca2e64d237110832327e8818739759e08328d91
minhajspace <minhaj.space@gmail.com>
2020-01-20 19:42:50 +0530

    adding increment method using hooks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react'
 
 const Counter = () => {
+    const [counter, setCounter] = useState(0)
     return (
         <div className="ui cards">
             <div className="card">
                 <div className="content ">
 
-                    <h1><center></center></h1>
+                    <h1><center>{counter}</center></h1>
 
                 </div>
 
 
                 <div className="extra content">
                     <div className="ui two buttons">
-                        <div className="ui basic green button" >Increment</div>
+                        <div className="ui basic green button" onClick={() => setCounter(counter + 1)} >Increment</div>
                         <div className="ui basic red button" >Decrement</div>
 
 
